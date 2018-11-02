c46a64e31b2c126613137268d085e4a9c1b3306e
Lei Zhu <zlheui2@gmail.com>
2018-11-03 00:00:51 +0800

    no idea indentation error

--- rafiki/feedback/feedback.py
+++ rafiki/feedback/feedback.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 class Feedback(object):
+
     def __init__(self, db=Database()):
         self._db = db
 
@@ -21,17 +22,10 @@ class Feedback(object):
 
     	if label is not None:
     		self._db.connect()
-    		feedback = self._db.create_feedback(
-                query_id = query_id, 
-                label = label
-            )
+    		feedback = self._db.create_feedback(query_id=query_id, label=label)
             self._db.commit()
             label_is_none = False
             is_added = True
         
-        return {
-            'query_id': query_id, 
-            'is_added': is_added, 
-            'label_is_none': label_is_none
-        } 
+        return {'query_id': query_id, 'is_added': is_added, 'label_is_none': label_is_none} 
 

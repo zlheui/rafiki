039874266a29637379c12554bfc0ce3dc8d08f51
Lei Zhu <zlheui2@gmail.com>
2018-11-17 18:43:10 +0800

    dispath training job to different drift detection worker

--- rafiki/feedback/feedback.py
+++ rafiki/feedback/feedback.py
@@ -37,7 +37,8 @@ class Feedback(object):
                 # send feedback to drift detector
                 running_drift_detection_worker_ids = self._cache.get_drift_detection_workers(ServiceType.DRIFT_FEEDBACK)
                 if len(running_drift_detection_worker_ids) > 0:
-                    con_drift_feedback_id = self._cache.add_feedback_of_worker(running_drift_detection_worker_ids[0], train_job_id, feedback.id, query_index, label)
+                    partition = train_job_id.int%len(running_drift_detection_worker_ids)
+                    con_drift_feedback_id = self._cache.add_feedback_of_worker(running_drift_detection_worker_ids[partition], train_job_id, feedback.id, query_index, label)
                 # send feedback to data repository
                 running_data_repository_worker_ids = self._cache.get_data_repository_workers(ServiceType.REPOSITORY_FEEDBACK)
                 if len(running_data_repository_worker_ids) > 0:
